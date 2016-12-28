378e3c7085649cd8adcc24e17dd56135a7a5f995
Kas Perch <the@nodebotani.st>
2016-12-27 22:15:50 -0600

    Started refactor

--- BusPirate.js
+++ BusPirate.js
@@ -1,51 +1,59 @@
 const util = require('util')
 const EventEmitter = require('events').EventEmitter
 const SerialPort = require('serialport')
+const async = require('async')
 
 const i2c = require('./lib/i2c.js')
 
-function BusPirate(options){
-  EventEmitter.call(this)
-  this.port = new SerialPort(
-    options.port,
-    {
-      baudRate: 115200,
-      //parser: SerialPort.parsers.readline('\n')
-    }
-  )
-  this._ready = false
+function BusPirate(options) {
+    EventEmitter.call(this)
+    this.port = new SerialPort(
+        options.port, {
+            baudRate: 115200,
+        }
+    )
+    this.inputQueue = []
+    this._ready = false
 
-  this.port.on('open', function(){ this.emit('open') }.bind(this))
+    this.port.on('open', function() { this.emit('open') }.bind(this))
 
-  this.port.on('data', function (data){
-    data = Buffer.from(data).toString()
-    console.log(data)
-    if(data.indexOf('BBIO1') !== -1){
-      this.ready()
-    }
-  }.bind(this))
+    this.port.on('data', function(data) {
+        data = Buffer.from(data).toString()
+        this.inputQueue.push(data)
+        console.log('Queue: ', this.inputQueue)
+    }.bind(this))
 }
 
 util.inherits(BusPirate, EventEmitter)
 
 Object.assign(BusPirate.prototype, i2c)
 
-BusPirate.prototype.reset = function(){
-  console.log('resetting')
-  this.port.write([0x0F])
+BusPirate.prototype.reset = function() {
+    this.port.write([0x0F])
 }
 
-BusPirate.prototype.start = function(){
-  console.log('entering bitbang mode')
-  this.port.write([0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00])
+BusPirate.prototype._flush = function() {
+    this.inputQueue = []
 }
 
-BusPirate.prototype.ready = function(){
-  if(!this._ready){
-    this.emit('ready')
-    this._ready = true
-  }
+BusPirate.prototype.start = function() {
+    async.until(
+        () => this._ready,
+        (cb) => {
+            if (this.inputQueue.length === 0) {
+                this.port.write([0x00], () => { setTimeout(cb, 100) })
+            } else {
+                let message = this.inputQueue.shift()
+                if (message.indexOf('BBIO1') !== -1) {
+                    console.log('ready')
+                    this._ready = true
+                    this.emit('ready')
+                    this._flush()
+                }
+                cb(null)
+            }
+        }
+    )
 }
-
 
 module.exports = BusPirate
--- examples/basic.js
+++ examples/basic.js
@@ -1,23 +1,25 @@
 var BusPirate = require('../BusPirate')
 
 var busPirate = new BusPirate({
-  port: '/dev/tty.usbmodem00000001'
+    port: '/dev/tty.usbserial-AI03KY7Z'
 })
 
 busPirate.on('open', () => {
-  busPirate.start()
+    busPirate.start()
 })
 
 busPirate.on('ready', () => {
-  console.log(busPirate)
-  busPirate.i2cInit()
-  busPirate.i2cConfig({
-    power: true
-  });
-  busPirate.i2cWrite(0x7e, [0x1e, 0x40])
+    busPirate.i2cInit(() => {
+        busPirate.i2cConfig({
+            power: true
+        }, () => {
+            console.log('writing')
+            busPirate.i2cWrite(0x7e, [0x1e, 0x40])
+        });
+    })
 })
 
-process.on('SIGINT', function(){
-  busPirate.reset()
-  process.exit()
+process.on('SIGINT', function() {
+    busPirate.reset()
+    process.exit()
 })
