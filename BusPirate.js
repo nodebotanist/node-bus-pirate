f830b3cb09e34fd3bc9e03d1bd7e15f2544f8abf
Kas Perch <the@nodebotani.st>
2016-12-31 01:15:10 -0600

    Added docs

--- BusPirate.js
+++ BusPirate.js
@@ -5,16 +5,35 @@ const async = require('async')
 
 const i2c = require('./lib/i2c.js')
 
+/** 
+ * Main BusPirate module
+ * @module BusPirate
+ * @author nodebotanist
+ */
+
+/**
+ * Represents a BusPirate object
+ * @constructor
+ * @function
+ * @param {Object} options
+ * @param {String} options.port -- the absolute path to the serial port of the bus pirate (e.g. /dev/tty.usbserial-xxxx)
+ */
 function BusPirate(options) {
     EventEmitter.call(this)
+
+    // Queue input from the bus pirate for stuff that needs to be synchronus
+    this.inputQueue = []
+
+    // Initial state setup
+    this._ready = false
+
     this.port = new SerialPort(
         options.port, {
             baudRate: 115200,
         }
     )
-    this.inputQueue = []
-    this._ready = false
 
+    // TODO: change these to () => syntax
     this.port.on('open', function() { this.emit('open') }.bind(this))
 
     this.port.on('data', function(data) {
@@ -26,8 +45,13 @@ function BusPirate(options) {
 
 util.inherits(BusPirate, EventEmitter)
 
+// Add in the I2C module
 Object.assign(BusPirate.prototype, i2c)
 
+/**
+ * Sends a reset code to the bus pirate
+ * @method reset
+ */
 BusPirate.prototype.reset = function() {
     this.port.write([0x0F])
 }
@@ -36,6 +60,11 @@ BusPirate.prototype._flush = function() {
     this.inputQueue = []
 }
 
+/**
+ * Starts the bus pirate.
+ * @method start
+ * @fires ready
+ */
 BusPirate.prototype.start = function() {
     async.until(
         () => this._ready,
@@ -47,6 +76,11 @@ BusPirate.prototype.start = function() {
                 if (message.indexOf('BBIO1') !== -1) {
                     console.log('ready')
                     this._ready = true
+                        /**
+                         * Ready event -- signals the bus pirate is ready to recieve commands
+                         * 
+                         * @event ready
+                         */
                     this.emit('ready')
                     this._flush()
                 }
