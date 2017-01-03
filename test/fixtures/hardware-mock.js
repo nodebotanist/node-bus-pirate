809d3747bf7fb690cfda4b47f30397b5f9836f8a
Kas Perch <the@nodebotani.st>
2017-01-02 19:31:05 -0600

    Added mock for serial port

--- test/fixtures/hardware-mock.js
+++ test/fixtures/hardware-mock.js
@@ -1,6 +1,22 @@
-// Need to add a mock that
-// stubs opening a serial port
-// stubs serial port writes
-// responds with sucess codes for various scenarios
-// responds with junk
-// responds with failure codes
+const EventEmitter = require('events').EventEmitter
+const util = require('util')
+
+// mock port
+// stubs all the functions of the serial port
+function MockPort() {
+    if (!this instanceof MockPort) {
+        throw new Error('MockPort is a constructor')
+    }
+}
+
+util.inherits(MockPort, EventEmitter)
+
+MockPort.prototype.open = () => {
+    this.emit('open')
+}
+
+MockPort.prototype.write = (dataArray, cb) => {
+    cb(null)
+}
+
+module.exports = MockPort
